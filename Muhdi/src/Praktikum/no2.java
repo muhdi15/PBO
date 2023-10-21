d10cb84f7e9bbfd3d552c031271fa8107c763ff7
Muhdi hell Code <115169961+muhdi15@users.noreply.github.com>
2023-10-21 22:15:56 +0800

    Update no2.java

--- Muhdi/src/Praktikum/no2.java
+++ Muhdi/src/Praktikum/no2.java
@@ -4,7 +4,7 @@ import java.util.Vector;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class no2 {
+public class no3 {
 
     public static void main(String[] args) {
         double[] data = {91.35, 4.72, 26, 3.5, 8, 61, 65.36, 12, 5.6, 7.11, 27, 9.53, 549, 2.23, 17.31, 4.25, 2.13, 83, 7, 102.4, 53.21, 3.42, 0.21, 70.02, 819.4, 6173, 4.25, 19.8, 17.35, 5.768};
